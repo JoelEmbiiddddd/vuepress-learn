a35dfb5d458d8b6284606226158c656eadb8fa8e
peterjxl <peterjxl@qq.com>
2023-02-18 17:01:13 +0800

    VuePress默认主题配置测试和拆分配置文件

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,10 @@
+import head from "./config/head"
+import themeConfig from "./config/themeConfig"
+import plugins from "./config/plugins"  
+module.exports = {
+  title: '网站标题',
+  theme: 'vdoing',
+  head,
+  themeConfig,
+  plugins
+}
---
+++ docs/.vuepress/config/themeConfig.ts
@@ -0,0 +1,30 @@
+import { VdoingThemeConfig } from "vuepress-theme-vdoing/types";
+
+export default <VdoingThemeConfig>{
+  logo: 'https://s3.bmp.ovh/imgs/2022/12/02/bc7428e3916c3a4c.jpg',
+  nav: [
+    { text: '首页', link: '/' },
+    { text: '计算机基础', link: '/CouputerBasic' },
+    {
+      text: 'Java', items: [
+        { text: 'JavaSE', link: '/JavaSE' },
+        { text: 'JavaEE', link: 'https://www.peterjxl.com/JavaEE' }
+      ]
+    },
+  ],
+  sidebar: 'structuring',
+
+  // 编辑此页配置
+  repo: 'Peter-JXL/vuepress-learn',
+  docsDir: 'docs',
+  editLinks: true,
+  editLinkText: '编辑此页',
+
+  // 页脚
+  footer: {
+    createYear: 2022,
+    copyrightInfo: `<a href='https://beian.miit.gov.cn'>粤ICP备2022067627号-1</a>  
+      <a href='http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44011302003646'>粤公网安备 44011302003646号</a>
+      `
+  },
+}
